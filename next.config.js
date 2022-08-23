29aaaf1a76bf4964dfbd1bf2da470ca6a283fa5f
Austin Witherow <au.witherow@gmail.com>
2022-08-23 15:35:55 -0400

    create basic redirect site version

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://forms.gle/k15C2vWyy1mkH5hv5",
+        permanent: true,
+      },
+      {
+        source: "/order",
+        destination: "https://forms.gle/k15C2vWyy1mkH5hv5",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
